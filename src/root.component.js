1c2825bebcc4e930977a2ef898e1f3d54770cb68
Aleksander Valle Grunnvoll <avgrunnvoll@gmail.com>
2021-10-03 20:37:10 +0200

    First

---
+++ src/root.component.js
@@ -0,0 +1,15 @@
+export default function Root(props) {
+  return (
+    <section
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "10rem",
+        fontSize: "3rem",
+      }}
+    >
+      This is the People microfrontend
+    </section>
+  );
+}
